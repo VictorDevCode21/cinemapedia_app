24c2580ffe499a7f020ac6a53cf50f9531729f42
Victor Rivas <victorenrique2002@gmail.com>
2024-03-23 23:22:40 -0400

    datasources and models created

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,9 +5,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Text(Environment.movieDBKey),
+        child: Placeholder(),
       ),
     );
   }
