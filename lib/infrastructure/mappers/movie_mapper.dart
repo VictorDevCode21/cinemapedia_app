91b84605c698c849fa15edc94796852f164f6850
Victor Rivas <victorenrique2002@gmail.com>
2024-03-23 23:45:51 -0400

    movie_mapper created

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: moviedb.backdropPath != ''
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          : 'https://edteam-media.s3.amazonaws.com/blogs/big/2ab53939-9b50-47dd-b56e-38d4ba3cc0f0.png',
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: moviedb.posterPath != ''
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+          : 'no-poster',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
+}
