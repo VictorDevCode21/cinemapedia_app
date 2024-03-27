cf4bc2272e4287dfff2f5ce67eb0311b5888ddf3
Victor Rivas <victorenrique2002@gmail.com>
2024-03-27 01:41:02 -0400

    implementations for the specific screen of movies and their details

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -16,6 +17,26 @@ class MovieMapper {
       posterPath: moviedb.posterPath != ''
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
           : 'no-poster',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
+
+  static Movie movieDetailsToEntity(MovieDetails moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: moviedb.backdropPath != ''
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          : 'https://edteam-media.s3.amazonaws.com/blogs/big/2ab53939-9b50-47dd-b56e-38d4ba3cc0f0.png',
+      genreIds: moviedb.genres.map((e) => e.name).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: moviedb.posterPath != ''
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+          : 'https://edteam-media.s3.amazonaws.com/blogs/big/2ab53939-9b50-47dd-b56e-38d4ba3cc0f0.png',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
