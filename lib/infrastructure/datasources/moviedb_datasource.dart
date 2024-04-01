35154425cee450a9588df188d729eb9980162f2c
Victor Rivas <victorenrique2002@gmail.com>
2024-04-01 01:21:17 -0400

    search functionality finished

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -70,4 +70,15 @@ class MovieDbDataSource extends MoviesDataSource {
 
     return movie;
   }
+
+  @override
+  Future<List<Movie>> searchMovies(String query) async {
+    if (query.isEmpty) return [];
+
+    final response = await dio.get('/search/movie', queryParameters: {
+      'query': query,
+    });
+
+    return _jsonToMovies(response.data);
+  }
 }
--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -16,7 +16,7 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: moviedb.posterPath != ''
           ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-          : 'no-poster',
+          : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoWcWg0E8pSjBNi0TtiZsqu8uD2PAr_K11DA&usqp=CAU',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
